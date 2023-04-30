a37bf55dd0ece19de9c88ab584ed65f58c68ca2a
Kazuki H <kazukih0205@gmail.com>
2023-04-30 21:02:51 +0900

    zsmalloc: Remove migrate_write_lock_nested()
    migrate_write_lock_nested() acts as a wrapper for write_lock_nested() which is exactly the
    same as write_lock() unless CONFIG_DEBUG_LOCKS_ALLOC is enabled. We're not debugging here.
    So, just remove migrate_write_lock_nested(). Downstream lacks write_lock_nested() anyway
    that causes compilation error.
    
    Fixes: ff0bf1f1ad ("BACKPORT: zsmalloc: replace per zpage lock with pool->migrate_lock")
    Fixes: e074609c48 ("BACKPORT: zsmalloc: rework compaction algorithm")
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>
    [ Tashar02: Reword the commit message and description ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -287,7 +287,6 @@ static void migrate_lock_init(struct zspage *zspage);
 static void migrate_read_lock(struct zspage *zspage);
 static void migrate_read_unlock(struct zspage *zspage);
 static void migrate_write_lock(struct zspage *zspage);
-static void migrate_write_lock_nested(struct zspage *zspage);
 static void migrate_write_unlock(struct zspage *zspage);
 static void kick_deferred_free(struct zs_pool *pool);
 static void init_deferred_free(struct zs_pool *pool);
@@ -301,7 +300,6 @@ static void migrate_lock_init(struct zspage *zspage) {}
 static void migrate_read_lock(struct zspage *zspage) {}
 static void migrate_read_unlock(struct zspage *zspage) {}
 static void migrate_write_lock(struct zspage *zspage) {}
-static void migrate_write_lock_nested(struct zspage *zspage) {}
 static void migrate_write_unlock(struct zspage *zspage) {}
 static void kick_deferred_free(struct zs_pool *pool) {}
 static void init_deferred_free(struct zs_pool *pool) {}
@@ -1765,11 +1763,6 @@ static void migrate_write_lock(struct zspage *zspage)
 	write_lock(&zspage->lock);
 }
 
-static void migrate_write_lock_nested(struct zspage *zspage)
-{
-	write_lock_nested(&zspage->lock, SINGLE_DEPTH_NESTING);
-}
-
 static void migrate_write_unlock(struct zspage *zspage)
 {
 	write_unlock(&zspage->lock);
@@ -2057,7 +2050,7 @@ static unsigned long __zs_compact(struct zs_pool *pool,
 		if (!src_zspage)
 			break;
 
-		migrate_write_lock_nested(src_zspage);
+		migrate_write_lock(src_zspage);
 
 		cc.obj_idx = 0;
 		cc.s_page = get_first_page(src_zspage);
