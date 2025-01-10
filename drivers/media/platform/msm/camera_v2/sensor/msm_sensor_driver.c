fe8a21b19f735f89b3ee1ed4a8a9cb2069891112
Sabar <31942277+sabarop@users.noreply.github.com>
2025-01-10 08:48:33 +0700

    msm: camera_v2: Import Platina changes

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -859,6 +859,10 @@ int32_t msm_sensor_driver_probe(void *setting,
 
 		slave_info->i2c_freq_mode = slave_info32->i2c_freq_mode;
 		slave_info->sensor_id_info = slave_info32->sensor_id_info;
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
+		slave_info->vendor_id_info = slave_info32->vendor_id_info;
+		slave_info->vcm_id_info = slave_info32->vcm_id_info;
+#endif
 
 		slave_info->slave_addr = slave_info32->slave_addr;
 		slave_info->power_setting_array.size =
@@ -965,7 +969,13 @@ int32_t msm_sensor_driver_probe(void *setting,
 		 */
 		if (slave_info->sensor_id_info.sensor_id ==
 			s_ctrl->sensordata->cam_slave_info->sensor_id_info
-			.sensor_id && !(strcmp(slave_info->sensor_name,
+			.sensor_id &&
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_PLATINA)
+			slave_info->vendor_id_info.vendor_id ==
+			s_ctrl->sensordata->cam_slave_info->
+				vendor_id_info.vendor_id &&
+#endif
+			!(strcmp(slave_info->sensor_name,
 			s_ctrl->sensordata->cam_slave_info->sensor_name))) {
 			pr_err("slot%d: sensor name: %s sensor id%d already probed\n",
 				slave_info->camera_id,
@@ -1066,6 +1076,11 @@ CSID_TG:
 	s_ctrl->sensordata->actuator_name = slave_info->actuator_name;
 	s_ctrl->sensordata->ois_name = slave_info->ois_name;
 	s_ctrl->sensordata->flash_name = slave_info->flash_name;
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
+	s_ctrl->sensordata->vendor_id_info = &(slave_info->vendor_id_info);
+	s_ctrl->sensordata->vcm_id_info = &(slave_info->vcm_id_info);
+#endif
+
 	/*
 	 * Update eeporm subdevice Id by input eeprom name
 	 */
