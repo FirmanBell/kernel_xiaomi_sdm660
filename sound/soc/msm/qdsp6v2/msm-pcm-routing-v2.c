ArtSb2005/android_kernel_xiaomi_sdm660

sound: msm: qdsp6v2: Import platina changes
Change-Id: Ie23f2192812bc288c018ab29ab1aa211c09a74db

sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -4135,7 +4135,7 @@ static const struct snd_kcontrol_new ext_ec_ref_mux_ul29 =
 static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 				  struct snd_ctl_elem_value *ucontrol)
 {
-#ifdef CONFIG_MACH_MI
+#ifdef CONFIG_MACH_XIAOMI_JASON
 	struct snd_soc_dapm_widget_list *wlist =
 						dapm_kcontrol_get_wlist(kcontrol);
 	struct snd_soc_dapm_widget *widget = wlist->widgets[0];
@@ -4143,7 +4143,7 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 	pr_debug("%s: ext_ec_ref_rx  = %x\n", __func__, msm_route_ext_ec_ref);
 
 	mutex_lock(&routing_lock);
-#ifdef CONFIG_MACH_MI
+#ifdef CONFIG_MACH_XIAOMI_JASON
 	if (!strncmp(widget->name, "VOC_EXT_EC MUX", strlen("VOC_EXT_EC MUX")))
 		ucontrol->value.integer.value[0] = voice_ext_ec_ref;
 	else
@@ -4228,7 +4228,7 @@ static int msm_routing_ext_ec_put(struct snd_kcontrol *kcontrol,
 		snd_soc_dapm_mux_update_power(widget->dapm, kcontrol, mux, e, update);
 #endif
 	} else {
-#ifndef CONFIG_MACH_MI
+#ifndef CONFIG_MACH_XIAOMI_JASON
 		ret = -EINVAL;
 #endif
 		mutex_unlock(&routing_lock);

FirmanBell/kernel_xiaomi_sdm660

sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -4145,7 +4145,7 @@ static const struct snd_kcontrol_new ext_ec_ref_mux_ul29 =
 static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 				  struct snd_ctl_elem_value *ucontrol)
 {
-#if defined(CONFIG_MACH_XIAOMI_MSM8998) || defined(CONFIG_MACH_XIAOMI_PLATINA)
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
 	struct snd_soc_dapm_widget_list *wlist =
 						dapm_kcontrol_get_wlist(kcontrol);
 	struct snd_soc_dapm_widget *widget = wlist->widgets[0];
@@ -4154,7 +4154,7 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 	pr_debug("%s: ext_ec_ref_rx  = %x\n", __func__, msm_route_ext_ec_ref);
 
 	mutex_lock(&routing_lock);
-#if defined(CONFIG_MACH_XIAOMI_MSM8998) || defined(CONFIG_MACH_XIAOMI_PLATINA)
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
 	if (!strncmp(widget->name, "VOC_EXT_EC MUX", strlen("VOC_EXT_EC MUX")))
 		ucontrol->value.integer.value[0] = voice_ext_ec_ref;
 	else
@@ -4248,9 +4248,7 @@ static int msm_routing_ext_ec_put(struct snd_kcontrol *kcontrol,
 		snd_soc_dapm_mux_update_power(widget->dapm, kcontrol, mux, e, update);
 #endif
 	} else {
-#ifndef CONFIG_MACH_XIAOMI_PLATINA
 		ret = -EINVAL;
-#endif
 		mutex_unlock(&routing_lock);
 	}
 