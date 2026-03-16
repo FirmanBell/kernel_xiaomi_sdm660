ac5c71e58c51cfa8906d96868ab069d0f730fdf9
rsuntk <rissu.ntk@gmail.com>
2026-03-16 14:59:04 +0700

    lockdep: fix too few args
    Change-Id: I07554df33ef5842771047d3fdbaeb9ded3811f85
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -476,12 +476,12 @@ static inline void lockdep_set_selftest_task(struct task_struct *task)
 {
 }
 
-# define lock_acquire(l, s, t, r, c, n, i)	((void)0)
-# define lock_release(l, n, i)			((void)0)
-# define lock_downgrade(l, i)			((void)0)
-# define lock_set_class(l, n, k, s, i)		((void)0)
-# define lock_set_subclass(l, s, i)		((void)0)
-# define lockdep_init()				((void)0)
+# define lock_acquire(l, s, t, r, c, n, i)	do { } while (0)
+# define lock_release(l, i)			do { } while (0)
+# define lock_downgrade(l, i)			do { } while (0)
+# define lock_set_class(l, n, k, s, i)		do { } while (0)
+# define lock_set_subclass(l, s, i)		do { } while (0)
+# define lockdep_init()				do { } while (0)
 # define lockdep_init_map_waits(lock, name, key, sub, inner, outer) \
 		do { (void)(name); (void)(key); } while (0)
 # define lockdep_init_map_wait(lock, name, key, sub, inner) \
@@ -715,8 +715,6 @@ do {									\
 # define might_lock_read(lock) do { } while (0)
 # define lockdep_assert_irqs_enabled() do { } while (0)
 # define lockdep_assert_irqs_disabled() do { } while (0)
-# define lockdep_assert_preemption_enabled() do { } while (0)
-# define lockdep_assert_preemption_disabled() do { } while (0)
 #endif
 
 #ifdef CONFIG_LOCKDEP
