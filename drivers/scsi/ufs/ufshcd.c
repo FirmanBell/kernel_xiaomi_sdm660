3d15abda122f8f7018b8221dbb813db1af1caf67
Cosmin Tanislav <demonsingur@gmail.com>
2026-04-25 09:39:03 +0800

    scsi/ufs/ufshcd: Increase doorbell wait time to 1.5secs
    Change-Id: I062a1bf9f4f9622e460f0288f48136d84ab78fe8

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1722,7 +1722,7 @@ out:
 
 static int ufshcd_clock_scaling_prepare(struct ufs_hba *hba)
 {
-	#define DOORBELL_CLR_TOUT_US		(1000 * 1000) /* 1 sec */
+	#define DOORBELL_CLR_TOUT_US		(1000 * 1500) /* 1.5 secs */
 	int ret = 0;
 	/*
 	 * make sure that there are no outstanding requests when
