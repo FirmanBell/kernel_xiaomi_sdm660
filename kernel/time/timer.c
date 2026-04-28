20a3f8593b43a1db6434541794d9981779a6f8fa
ris <rissu.ntk@gmail.com>
2026-04-28 22:20:54 +0700

    time: remove non-existing lockdep_assert_preemption_enabled
    * lockdep_assert_preemption_enabled now is considered as implicit
    
    Change-Id: I26422fc7ed7247babce8352ae273ff82b4b9ec08
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1407,13 +1407,6 @@ int del_timer_sync(struct timer_list *timer)
 	 */
 	WARN_ON(in_irq() && !(timer->flags & TIMER_IRQSAFE));
 
-	/*
-	 * Must be able to sleep on PREEMPT_RT because of the slowpath in
-	 * del_timer_wait_running().
-	 */
-	if (IS_ENABLED(CONFIG_PREEMPT_RT) && !(timer->flags & TIMER_IRQSAFE))
-		lockdep_assert_preemption_enabled();
-
 	do {
 		ret = try_to_del_timer_sync(timer);
 
